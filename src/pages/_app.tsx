392f258a1c6eea3789c4d64b0dc06bec035e7571
정해선 <haessun@haessun.local>
2024-06-19 16:33:07 +0900

    google analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,32 @@ import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
+import * as gtag from "../lib/gtag";
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
+
 /**
  * @description SEO를 위해 본인의 정보로 수정해주세요.
  */
+
 const DEFAULT_SEO = {
   title: "정해선 | Music AI Researcher",
-  description: "안녕하세요, 프론트엔드 개발자 홍길동입니다.",
+  description: "안녕하세요, 정해선입니다. 음악과 인공지능을 연구하는 연구원입니다.",
   canonical: "https://www.naver.com/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    title: "정해선 | Music AI Researcher",
+    site_name: "정해선 | Music AI Researcher",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "홍길동 | Front-End Dev",
+        alt: "정해선 | Music AI Researcher",
       },
     ],
   },
@@ -34,11 +41,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "홍길동 | Front-End Dev",
+      content: "정해선 | Music AI Researcher",
     },
     {
       name: "msapplication-tooltip",
-      content: "홍길동 | Front-End Dev",
+      content: "정해선 | Music AI Researcher",
     },
     {
       name: "viewport",
@@ -48,8 +55,41 @@ const DEFAULT_SEO = {
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
       <DefaultSeo {...DEFAULT_SEO} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />

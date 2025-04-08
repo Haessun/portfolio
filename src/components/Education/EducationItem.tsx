111076f6b89adf5bab4fd92fdd72cd16bda7ad34
정해선 <haessun@haessun.local>
2025-04-08 17:53:20 +0900

    modify education item.tsx - font issue

--- src/components/Education/EducationItem.tsx
+++ src/components/Education/EducationItem.tsx
@@ -5,12 +5,19 @@ const EducationItem = ({ name, description, period, thesis, researchArea }: {
   thesis?: string;
   researchArea?: string;
 }) => {
+  const [title, school] = name.split(" @ ");
+
   return (
     // <div className="flex flex-col md:flex-row gap-2 md:gap-1 reduced-spacing">
-    <div className="flex flex-col md:flex-row gap-2 md:gap-1">
+    <div className="flex flex-col md:flex-row gap-2 md:gap-1 mb-4">
       <div className="flex flex-col gap-1">
         <div className="flex flex-col">
-          <h4>{name}</h4>
+          <h4>
+            {title}
+            {school && (
+              <span className="ml-1 text-sm italic text-gray-500">@ {school}</span>
+            )}
+          </h4>
           <span>{`${period[0]}${period[1] ? " - " + period[1] : ""}`}</span>
         </div>
         {description && <span className="whitespace-pre-wrap">{description}</span>}

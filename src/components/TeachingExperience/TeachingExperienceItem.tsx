12c482d1173456ab3fd11b940beaebd4956359e8
정해선 <haessun@haessun.local>
2025-04-09 16:54:41 +0900

    fix teaching exp. 2

--- src/components/TeachingExperience/TeachingExperienceItem.tsx
+++ src/components/TeachingExperience/TeachingExperienceItem.tsx
@@ -3,11 +3,11 @@ import { TeachingExperienceProps } from "@/types";
 const TeachingExperienceItem = ({ name, lecture, period }: TeachingExperienceProps) => {
   return (
     <div className="flex flex-col mb-4">
-      <h4>{lecture}</h4>
+      <h5>{lecture}</h5>
 
       <div className="flex justify-between items-center w-full">
-        <span className="font-medium text-base">{name}</span>
-        <span className="text-sm text-gray-500">{`${period[0]} - ${period[1]}`}</span>
+        <span>{name}</span>
+        <span className="text-gray-500">{`${period[0]} - ${period[1]}`}</span>
       </div>
     </div>
   );

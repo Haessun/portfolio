02d05e2d7ee5a557b35d62a863d4279a22ee459e
정해선 <haessun@haessun.local>
2026-01-22 15:58:42 +0900

    fix leadershipItem part

--- src/components/LeadershipExperience/LeadershipExperienceItem.tsx
+++ src/components/LeadershipExperience/LeadershipExperienceItem.tsx
@@ -2,12 +2,14 @@ import { LeadershipExperienceProps } from "@/types";
 
 const LeadershipExperienceItem = ({ name, lecture, period }: LeadershipExperienceProps) => {
   return (
-    <div className="flex flex-col mb-4">
+    <div className="flex flex-col mb-4 text-sm">
       <h5>{lecture}</h5>
 
-      <div className="flex justify-between items-center w-full">
-        <span>{name}</span>
-        <span className="text-gray-500">{`${period[0]} - ${period[1]}`}</span>
+      <div className="flex justify-between items-center w-full gap-4">
+        <span className="truncate">{name}</span>
+        <span className="text-gray-500 whitespace-nowrap flex-shrink-0">
+          {period.length === 1 ? period[0] : `${period[0]} - ${period[1]}`}
+        </span>
       </div>
     </div>
   );

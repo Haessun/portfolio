080115b0e7590ac38171102f221dd62f2a86960f
정해선 <haessun@haessun.local>
2026-01-22 18:58:42 +0900

    fix leadership.tsx font, italic

--- src/components/LeadershipExperience/LeadershipExperienceItem.tsx
+++ src/components/LeadershipExperience/LeadershipExperienceItem.tsx
@@ -3,10 +3,10 @@ import { LeadershipExperienceProps } from "@/types";
 const LeadershipExperienceItem = ({ name, lecture, period }: LeadershipExperienceProps) => {
   return (
     <div className="flex flex-col mb-2 text-sm">
-      <h5 style={{ fontSize: '15px' }}>{lecture}</h5>
+      <h5 style={{ fontSize: '16px' }}>{lecture}</h5>
 
       <div className="grid grid-cols-[1fr_160px] gap-4 w-full">
-        <span className="break-words">{name}</span>
+        <span className="break-words italic">{name}</span>
         <span className="text-gray-500 whitespace-nowrap text-right">
           {period.length === 1 ? period[0] : `${period[0]} - ${period[1]}`}
         </span>
